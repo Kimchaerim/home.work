931ec1736fb18ba87b367b748232b81cb8b21003
kimchaerim <201812075@skhu.net>
2020-05-24 19:39:17 +0900

    [웹프]10주차 과

---
+++ lecture1/src/lecture1/jdbc1/ProfessorDAO.java
@@ -0,0 +1,26 @@
+package lecture1.jdbc1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfessorDAO {
+
+	public static List<Professor> findAll() throws Exception {
+		String sql = "SELECT * FROM professor";
+		try (Connection connection = DB.getConnection("student1");
+				PreparedStatement statement = connection.prepareStatement(sql);
+				ResultSet resultSet = statement.executeQuery()) {
+			ArrayList<Professor> list = new ArrayList<>();
+			while (resultSet.next()) {
+				Professor professor = new Professor();
+				professor.setId(resultSet.getInt("id"));
+				professor.setProfessorName(resultSet.getString("professorName"));
+				list.add(professor);
+			}
+			return list;
+		}
+	}
+}
